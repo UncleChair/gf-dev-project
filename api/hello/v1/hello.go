400c95f7f40efee187fc1f1e2f709160f568d2f9
Jonny Kan <voidgun1998@gmail.com>
2024-10-16 13:33:09 +0800

    Add

--- api/hello/v1/hello.go
+++ api/hello/v1/hello.go
@@ -2,18 +2,42 @@ package v1
 
 import (
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/goai"
 )
 
 type HelloReq struct {
 	g.Meta `path:"/ping" tags:"Admin API" method:"get" sum:"Server Ping Check"`
 }
 type HelloRes struct {
-	g.Meta      `status:"201"`
-	Status      string    `json:"status" des:"server status" eg:"OK!"`
-	StatusError *Error404 `status:"403" `
+	g.Meta `status:"201" resEg:"resource/test.json"`
+	Status string `json:"status" des:"server status" eg:"OK!"`
 }
 
-type Error404 struct {
-	Code    string `json:"code" eg:"1"`
-	Message string `json:"message" eg:"Not Found"`
+// Example case
+type Issue3747Res401 struct {
+	g.Meta `resEg:"resource/test.json"`
+}
+
+// Override case 1
+type Issue3747Res402 struct {
+	g.Meta `mime:"application/json"`
+}
+
+// Override case 2
+type Issue3747Res403 struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// Common response case
+type Issue3747Res404 struct{}
+
+func (r HelloRes) ResponseStatusMap() map[goai.StatusCode]any {
+	return map[goai.StatusCode]any{
+		401: Issue3747Res401{},
+		402: Issue3747Res402{},
+		403: Issue3747Res403{},
+		404: Issue3747Res404{},
+		406: struct{}{},
+	}
 }
